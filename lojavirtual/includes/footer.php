d88625ea44312d1b79b54a4fbaf07b5ea03efe7d
MuriloGB <105112352+Murilo-hub@users.noreply.github.com>
2025-05-21 23:02:27 -0300

    Delete lojavirtual directory

--- lojavirtual/includes/footer.php
+++
@@ -1,5 +0,0 @@
-<!-- Rodapé da página -->
-<footer>
-    <p>&copy; 2025 Loja Virtual. Todos os direitos reservados.</p>
-    <p><a href="termos.php">Termos e Condições</a> | <a href="privacidade.php">Política de Privacidade</a></p>
-</footer>
--- lojavirtual/includes/header.php
+++
@@ -1,5 +0,0 @@
-<!-- Cabeçalho com barra de navegação -->
-<header>
-    <h1>Loja Virtual</h1>
-    <?php include_once('../includes/barranav.php'); ?>
-</header>
