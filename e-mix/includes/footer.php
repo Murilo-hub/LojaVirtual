dc0bab4492b30575a8a0e8711a450ab8fc6763d2
MuriloGB <105112352+Murilo-hub@users.noreply.github.com>
2025-05-21 23:03:52 -0300

    Update footer.php

--- e-mix/includes/footer.php
+++ e-mix/includes/footer.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="../publico/css/style.css">
 </head>
+<html>
 <body>
   <footer class="footer">
     <p>&copy; 2025 E-mix. Todos os direitos reservados.</p>
